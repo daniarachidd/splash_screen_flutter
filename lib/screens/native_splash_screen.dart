0f881c023a784011cb524b8fa7626cbc03cc5241
Dania Rachid <rachidd-wp18@student.tarc.edu.my>
2022-11-08 17:52:54 +0300

    Initial commit

---
+++ lib/screens/native_splash_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:flutter/material.dart';
+
+class NativeSplash extends StatelessWidget {
+  const NativeSplash({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    body: Center(
+      child: Text('Native Splash Screen')
+    )
+    );
+  }
+}
